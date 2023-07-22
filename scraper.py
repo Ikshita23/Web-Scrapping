8d398d38b6efc3e50baf28dca7d618d2665f56d9
Ikshita23 <109181236+Ikshita23@users.noreply.github.com>
2023-07-22 23:16:02 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,21 +1,15 @@
 from decouple import config, Csv
-from boards import indeed, monster
+from boards import indeed
 
-monster_url = config('MONSTER_URL')
 indeed_url = config('INDEED_URL')
 disgard_list = config('DESCRIPTION_DOESNT_CONTAIN', cast=Csv())
-
-print('Getting jobs from monster.com')
-monster = monster.MonsterJobs(monster_url)
-monster_jobs = monster.get()
-print(f'Found {len(monster_jobs)} job listings within the specified criteria')
 
 print('Getting jobs from indeed.com')
 indeed = indeed.IndeedJobs(indeed_url)
 indeed_jobs = indeed.get()
-print(f'Found {len(monster_jobs)} job listings within the specified criteria')
+print(f'Found {len(indeed_jobs)} job listings within the specified criteria')
 
-unfiltered_jobs = monster_jobs + indeed_jobs
+unfiltered_jobs =  indeed_jobs
 filtered_jobs = []
 
 for job in unfiltered_jobs:
